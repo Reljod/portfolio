8f30cc27bd0439947004b4b069f295bb643f7db2
Reljod Oreta <oretareljod@gmail.com>
2022-03-27 11:24:00 +0800

    Add Pi authentication at developer or sandbox mode

---
+++ src/components/NoAuth.js
@@ -0,0 +1,10 @@
+
+import '../styles/App.css'
+
+export function NoAuth() {
+  return (
+    <div className="NoAuth">
+      Pi authentication only supports Developer or Sandbox mode.
+    </div>
+  )
+}
--- src/components/PiAuth.js
+++ src/components/PiAuth.js
@@ -1,59 +1,73 @@
-// import { Pi } from '@pinetwork-js/sdk';
-import { useState, useCallback, useEffect } from 'react';
+import { Pi } from '@pinetwork-js/sdk';
+import { useState, useEffect } from 'react';
+import { getUser, login } from '../api/auth';
 import '../styles/PiAuth.css'
+import { useQuery } from 'react-query';
 
+Pi.init({ version: '2.0', sandbox: true });
+
+function formWelcomeText(username) {
+  return `Welcome to the App, ${username}`;
+} 
 
 export default function Auth() {
 
+  const {isLoading, data} = useQuery('getUser', getUser);
+
   const [isLoggedIn, setLogged] = useState(false);
+  const [user, setUser] = useState({});
   const [formTitle, setFormTitle] = useState('Authenticate with Pi!');
   const [buttonClass, setButtonClass] = useState('AuthButtonActive');
 
-  // Pi.init({ version: '2.0'});
-
-  // const authenticate = () => {
-  //   const scopes = ['payments'];
-  
-  //   const onIncompletePaymentFound = (payment) => {
-  //     alert("Incomplete Payment Found");
-  //     return;
-  //   }
-  
-  //   Pi.authenticate(scopes, onIncompletePaymentFound).then(
-  //     (auth) => {
-  //       const { username } = auth.user;
-  //       const authFormTitle = `Welcome to the App, ${username}`;
-  //       setFormTitle(authFormTitle);
-  //       console.log(auth);
-  //   }).catch(
-  //     (error) => {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
-
   useEffect(() => {
     if (!isLoggedIn) {
-      setButtonClass('AuthButtonActive')
+      setButtonClass('AuthButtonActive');
     } else {
-      setButtonClass('AuthButtonDisabled')
+      setButtonClass('AuthButtonDisabled');
+      setFormTitle(formWelcomeText(user.username));
     }
-  }, [isLoggedIn])
+  }, [isLoggedIn, user]);
 
-  const onAuthenticate = useCallback(
-    (title) => {
-      setFormTitle(title);
-      setLogged(true);
-      console.log("Is logged in?:", isLoggedIn);
+  useEffect(() => {
+    if (!isLoading) {
+      console.log(data);
+      setUser({...data});
+      if (data.hasToken){
+        setLogged(true);
+      }
     }
-  , [isLoggedIn]);
+  }, [isLoading, data]);
+
+  const authenticate = () => {
+    const scopes = ['username','payments'];
+  
+    const onIncompletePaymentFound = (payment) => {
+      alert("Incomplete Payment Found");
+      return;
+    }
+  
+    Pi.authenticate(scopes, onIncompletePaymentFound).then(
+      (auth) => {
+        const { username } = auth.user;
+        const authFormTitle = formWelcomeText(username);
+        setFormTitle(authFormTitle);
+        setUser({...auth.user});
+        setLogged(true);
+        login(auth.accessToken);
+        console.log(auth);
+    }).catch(
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   return (
     <>
       <p className='AuthTitle'>{ formTitle }</p>
       <button 
         className={'AuthButton ' + buttonClass}
-        onClick={() => onAuthenticate("Welcome to Pi, Reljod!")}
+        onClick={authenticate}
         disabled={isLoggedIn}
       >
         <p>Authenticate</p>
